19fdff514484d6907f3480bd6061f061945d8685
winprn <minhladz001@gmail.com>
2023-10-22 15:21:50 +0700

    refactor InputHandler

--- PuppyCrossingGame/InputHandler.cpp
+++ PuppyCrossingGame/InputHandler.cpp
@@ -1,17 +1,15 @@
 #include "InputHandler.h"
 
+InputHandler::InputHandler() {
+	move_up = new MoveUpCommand;
+}
+
 Command* InputHandler::handleInput(WPARAM key)
 {
 	switch (key)
 	{
 	case VK_UP:
 		return move_up;
-	case VK_DOWN:
-		return move_down;
-	case VK_LEFT:
-		return move_left;
-	case VK_RIGHT:
-		return move_right;
 	default:
 		return nullptr;
 	}
--- PuppyCrossingGame/InputHandler.h
+++ PuppyCrossingGame/InputHandler.h
@@ -1,18 +1,13 @@
 #pragma once
 #include "Command.h"
+#include "MoveUpCommand.h"
 class InputHandler
 {
 public:
+	InputHandler();
+
 	Command* handleInput(WPARAM key);
-	void set_move_up(Command* c) {
-		move_up = c;
-	}
 private:
-	Command* move_up;
-	Command* move_down;
-	Command* move_left;
-	Command* move_right;
-	Command* change_skin;
-	Command* pause;
+	MoveUpCommand* move_up;
 };
 
