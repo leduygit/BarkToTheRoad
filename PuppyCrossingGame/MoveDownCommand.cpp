21f9536d5d1aa05e9b24fb3208e17cab60c8d0c7
winprn <minhladz001@gmail.com>
2023-10-27 23:18:02 +0700

    remove old code

--- PuppyCrossingGame/MoveDownCommand.cpp
+++ PuppyCrossingGame/MoveDownCommand.cpp
@@ -11,7 +11,6 @@ bool MoveDownCommand::isValidMove(Character& c, Map& m)
 {
 	c.setPos({ c.getPos().X, static_cast<short>(c.getPos().Y - 90) });
 	bool is_collision = m.checkCollisionWithStaticObstacle(c);
-	bool is_collision = m.checkCollision(c);
 	c.setPos({ c.getPos().X, static_cast<short>(c.getPos().Y + 90) });
 
 	return !is_collision;
