d78374b7b92b23222108ae9ff48f0ee3e7950d5f
KietHuynh <hctuankiet243@gmail.com>
2023-11-24 14:12:24 +0700

    fix pause game bug

--- PuppyCrossingGame/LoadShape.h
+++ PuppyCrossingGame/LoadShape.h
@@ -6,7 +6,7 @@ enum ShapeIndex
 	HCTKCODECAINAY, CAR_RIGHT, CAR_LEFT, GRASS_0,
 	GRASS_1, GRASS_2, GRASS_3, GRASS_4,
 	STONE, STONE_1, BUSH_2, ROAD, TRAIN_RIGHT, TRAIN_LEFT, 
-	DOG_STAY_1, DOG_STAY_2, DOG_JUMP_1, DOG_JUMP_2, 
+	DOG_STAY_1, DOG_STAY_2, DOG_JUMP_1, DOG_JUMP_2, DOG_DIE,
 	RAIL, LOG, RIVER, RED_LIGHT, GREEN_LIGHT, 
 	BACKGROUND, NEW_GAME_BUTTON, NEW_GAME_BUTTON_HOVER,
 	LOAD_GAME_BUTTON, LOAD_GAME_BUTTON_HOVER,
@@ -16,5 +16,6 @@ enum ShapeIndex
 };
 extern std::vector<Shape*> DogMovingShapes;
 extern std::vector<Shape*> DogStayingShapes;
+extern std::vector<Shape*> DogDyingShapes;
 extern std::vector<Shape*> MyShape;
 void initShape();
