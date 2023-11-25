dfdcec7c26bdf95bb8accd521e3d7190953b8740
winprn <minhladz001@gmail.com>
2023-11-25 13:08:04 +0700

    fix: rank screen not update latest score.txt after game finish

--- PuppyCrossingGame/RankScreen.h
+++ PuppyCrossingGame/RankScreen.h
@@ -1,13 +1,22 @@
 #pragma once
 #include "Screen.h"
 #include <algorithm>
+#include <filesystem>
 
 struct User {
 	std::string name;
-	int score;
+	std::string score;
 };
 
 bool cmp(const User& a, const User& b);
+template <typename TP>
+std::time_t to_time_t(TP tp)
+{
+	using namespace std::chrono;
+	auto sctp = time_point_cast<system_clock::duration>(tp - TP::clock::now()
+		+ system_clock::now());
+	return system_clock::to_time_t(sctp);
+}
 
 class RankScreen : public Screen
 {
@@ -21,5 +30,6 @@ private:
 	std::vector<User> m_users{};
 	std::string m_key{ "" };
 	Shape* m_board{};
+	time_t ftime{};
 };
 
